8b18efab07dcda57b7f8eb3d8b4c723c23ab6585
hq-mac <1076084189@qq.com>
2026-06-17 10:37:14 +0800

    feat: 封装response.py

--- src/modules/users/user_controller.py
+++ src/modules/users/user_controller.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
+import pydantic
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.common.response import success
 from src.config.db import get_db
-from src.modules.users.user_dto import UserCreate, UserUpdate
+from src.modules.users.user_dto import UserCreate, UserResp, UserUpdate
 from src.modules.users.user_service import UserService
 
 router = APIRouter(prefix = '/users')
@@ -12,31 +14,41 @@ def get_svc():
 
 @router.post("")
 async def create_user(payload: UserCreate, db: AsyncSession = Depends(get_db), svc: UserService = Depends(get_svc)):
-    return await svc.create(db, payload)
+    new_user = await svc.create(db, payload)
+    new_pydantic_user = UserResp.model_validate(new_user)
+    return success(data=new_pydantic_user.model_dump()) 
 
 @router.get("/{user_id}")
 async def get_user(user_id: int, db: AsyncSession = Depends(get_db), svc: UserService = Depends(get_svc)):
     user = await svc.get_by_id(user_id, db)
     if not user:
         raise HTTPException(status_code=400, detail="用户不存在")
-    return user
+    pydantic_user = UserResp.model_validate(user)
+    return success(data=pydantic_user.model_dump())
 
 @router.get("")
 async def get_all_users(db: AsyncSession = Depends(get_db), svc: UserService = Depends(get_svc)):
     users = await svc.get_all(db)
-    return users
+    # UserResp.model_validate(user).model_dump() for user in users
+    data = []
+    for user in users:
+        pydantic_user = UserResp.model_validate(user)
+        data.append(pydantic_user.model_dump())
+        
+    return success(data=data)
 
 @router.put("/{user_id}")
 async def update_user(user_id: int, payload: UserUpdate, svc: UserService = Depends(get_svc), db: AsyncSession = Depends(get_db)):
     user = await svc.update_by_id(user_id, payload, db)
     if not user:
         raise HTTPException(status_code=400, detail="用户不存在")
-    return user
+    pydantic_user = UserResp.model_validate(user)
+    return success(data=pydantic_user.model_dump())
 
 @router.delete("/{user_id}")
 def delete_user(user_id: int, svc: UserService = Depends(get_svc), db: AsyncSession = Depends(get_db)):
     success = svc.delete_by_id(user_id, db)
     if not success:
         raise HTTPException(status_code=400, detail="用户不存在或删除失败")
-    return {"message": "用户删除成功"}
+    return success(msg="删除用户成功")
 
--- src/modules/users/user_dto.py
+++ src/modules/users/user_dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class UserCreate(BaseModel):
     name: str
@@ -12,4 +12,9 @@ class UserResp(UserCreate):
     id: int
     create_time: str | None = None
     update_time: str | None = None
+    # 支持直接用 ORM 实例解析成 Pydantic 对象。
+    # ORM 实例：从数据库查出来的db_user，是 SQLAlchemy 封装的数据库对象，不能直接返回给前端。
+    # Pydantic 对象：用来规范返回给前端的数据结构。
+    # from_attributes=True：让UserResp能直接读取db_user里的字段，不用手动逐个赋值
+    model_config = ConfigDict(from_attributes=True)
 
